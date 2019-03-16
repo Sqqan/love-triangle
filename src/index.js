bddd5e9f3acadf0706e12b0d6bffe657f3ec0eb2
Sqqan <FondMoha@gmail.com>
2019-03-16 17:04:37 +0300

    feat: changed index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var length = preferences.length; 
+var count = 0; 
+for (let i = 0; i<length; i++) { 
+var love = preferences[i] - 1;
+if (love == i) continue;
+var lovelove = preferences[love] - 1;
+if (love == lovelove) continue;
+var lovelovelove = preferences[lovelove] - 1;
+if (lovelovelove == lovelove) continue;
+if (lovelovelove == i) {
+count++;
+}
+}
+return count / 3;
 };
